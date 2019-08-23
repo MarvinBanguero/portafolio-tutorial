c7b8e22d08438626223a4228a16e2cf3081986a8
Marvin Banguero <alejandrozapata140@gmail.com>
2019-08-23 14:46:48 -0500

    seccón de firebase terminada

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -7,27 +7,49 @@ import { InfoPagina } from '../interfaces/info-pagina';
 })
 export class InfoPaginaService {
 
+equipo:any=[];
 info: InfoPagina ={};
 cargada:true;
 
   constructor(private http: HttpClient) {
 
     console.log('Servicio InfoPagina Listo');
-    
-    this.http.get('assets/data/data-pagina.json')
-         .subscribe((resp: InfoPagina)=> {
-
-            //resp contendra toda la informacion del archivo json convertido en obj JavaScript
-            this.info=resp;
-            this.cargada=true;
-            console.log(resp);//de esta manera muestro todos los datos
-            //console.log(resp['email']); //de esta manera ,uestro las propiedades de interes
-            ////////////////////////////////////////////////////////
-           
-
-
-         } )      
+    this.cargarEquipo();
+    this.cargarInfo();
+         
 
     //leer mi archivo JSON
+   }
+
+   private cargarInfo(){
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe((resp: InfoPagina)=> {
+
+       //resp contendra toda la informacion del archivo json convertido en obj JavaScript
+       this.info=resp;
+       this.cargada=true;
+       console.log(resp);//de esta manera muestro todos los datos
+       //console.log(resp['email']); //de esta manera ,uestro las propiedades de interes
+       ////////////////////////////////////////////////////////
+      
+
+
+    } ) 
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-2c91a.firebaseio.com/equipo.json')
+    .subscribe((resp:any[])=> {
+
+       //resp contendra toda la informacion del archivo json convertido en obj JavaScript
+       this.equipo=resp;
+       this.cargada=true;
+       console.log(resp);//de esta manera muestro todos los datos
+       //console.log(resp['email']); //de esta manera ,uestro las propiedades de interes
+       ////////////////////////////////////////////////////////
+      
+
+
+    } ) 
    }
 }
